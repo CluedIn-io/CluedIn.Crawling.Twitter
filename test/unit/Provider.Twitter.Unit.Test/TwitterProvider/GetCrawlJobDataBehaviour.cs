318dbc40a6786f905885b3116ba7892791ec2736
Ziggy <zygis0408@gmail.com>
2020-07-08 10:19:28 +0200

    removed tests

--- test/unit/Provider.Twitter.Unit.Test/TwitterProvider/GetCrawlJobDataBehaviour.cs
+++ test/unit/Provider.Twitter.Unit.Test/TwitterProvider/GetCrawlJobDataBehaviour.cs
@@ -1,51 +1,51 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using AutoFixture.Xunit2;
-using CluedIn.Core.Crawling;
-using CluedIn.Core.Providers;
-using CluedIn.Crawling.Twitter.Core;
-using Xunit;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using AutoFixture.Xunit2;
+//using CluedIn.Core.Crawling;
+//using CluedIn.Core.Providers;
+//using CluedIn.Crawling.Twitter.Core;
+//using Xunit;
 
-namespace CluedIn.Provider.Twitter.Unit.Test.TwitterProvider
-{
-    public class GetCrawlJobDataBehaviour : TwitterProviderTest
-    {
-        private readonly ProviderUpdateContext _context;
+//namespace CluedIn.Provider.Twitter.Unit.Test.TwitterProvider
+//{
+//    public class GetCrawlJobDataBehaviour : TwitterProviderTest
+//    {
+//        private readonly ProviderUpdateContext _context;
 
-        public GetCrawlJobDataBehaviour()
-        {
-            _context = null;
-        }
+//        public GetCrawlJobDataBehaviour()
+//        {
+//            _context = null;
+//        }
 
-        [Theory]
-        [InlineAutoData]
-        public async Task ReturnsData(Dictionary<string, object> dictionary, Guid organizationId, Guid userId, Guid providerDefinitionId)
-        {
-            Assert.NotNull(
-                await Sut.GetCrawlJobData(_context, dictionary, organizationId, userId, providerDefinitionId));
-        }
+//        [Theory]
+//        [InlineAutoData]
+//        public async Task ReturnsData(Dictionary<string, object> dictionary, Guid organizationId, Guid userId, Guid providerDefinitionId)
+//        {
+//            Assert.NotNull(
+//                await Sut.GetCrawlJobData(_context, dictionary, organizationId, userId, providerDefinitionId));
+//        }
 
-        [Theory]
-        [InlineAutoData(TwitterConstants.KeyName.ApiKey, nameof(TwitterCrawlJobData.ApiKey))]
-        public async Task InitializesProperties(string key, string propertyName, string sampleValue, Guid organizationId, Guid userId, Guid providerDefinitionId)
-        {
-            var dictionary = new Dictionary<string, object>()
-            {
-                [key] = sampleValue
-            };
+//        [Theory]
+//        [InlineAutoData(TwitterConstants.KeyName.ApiKey, nameof(TwitterCrawlJobData.ApiKey))]
+//        public async Task InitializesProperties(string key, string propertyName, string sampleValue, Guid organizationId, Guid userId, Guid providerDefinitionId)
+//        {
+//            var dictionary = new Dictionary<string, object>()
+//            {
+//                [key] = sampleValue
+//            };
 
-            var sut = await Sut.GetCrawlJobData(_context, dictionary, organizationId, userId, providerDefinitionId);
-            Assert.Equal(sampleValue, sut.GetType().GetProperty(propertyName).GetValue(sut));
-        }
+//            var sut = await Sut.GetCrawlJobData(_context, dictionary, organizationId, userId, providerDefinitionId);
+//            Assert.Equal(sampleValue, sut.GetType().GetProperty(propertyName).GetValue(sut));
+//        }
 
-        [Theory]
-        [InlineAutoData]
-        public async Task TwitterCrawlJobDataReturned(Dictionary<string, object> dictionary, Guid organizationId, Guid userId, Guid providerDefinitionId)
-        {
-            Assert.IsType<CluedIn.Crawling.Twitter.Core.TwitterCrawlJobData>(
-                await Sut.GetCrawlJobData(_context, dictionary, organizationId, userId, providerDefinitionId));
-        }
-    }
-}
+//        [Theory]
+//        [InlineAutoData]
+//        public async Task TwitterCrawlJobDataReturned(Dictionary<string, object> dictionary, Guid organizationId, Guid userId, Guid providerDefinitionId)
+//        {
+//            Assert.IsType<CluedIn.Crawling.Twitter.Core.TwitterCrawlJobData>(
+//                await Sut.GetCrawlJobData(_context, dictionary, organizationId, userId, providerDefinitionId));
+//        }
+//    }
+//}
